904c7aac827103b734dd2cd893244b88a9818255
gutchom <yskz.tg@gmail.com>
2023-03-10 21:39:52 +0900

    add local https settings to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { readFileSync } from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,
+    https: {
+      key: readFileSync('../cert/chombook.local-key.pem'),
+      cert: readFileSync('../cert/chombook.local.pem'),
+    },
+  },
+});
